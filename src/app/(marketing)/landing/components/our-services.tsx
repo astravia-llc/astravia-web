8cea52d54315f18609bb311869ac93934916f382
“Jhon <jfacostamu@unal.edu.co>
2025-08-21 21:27:59 -0500

    fix: updated icons

--- src/app/(marketing)/landing/components/our-services.tsx
+++ src/app/(marketing)/landing/components/our-services.tsx
@@ -1,4 +1,10 @@
-import { ShieldCheck, FileLock, GlobeLock, Award, Key } from "lucide-react";
+import {
+  ShieldCheck,
+  Award,
+  Network,
+  BrainCircuit,
+  Handshake,
+} from "lucide-react";
 
 export function OurServices() {
   return (
@@ -11,23 +17,25 @@ export function OurServices() {
     >
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-16">
-          <h3 className="text-sm text-neutral-400 uppercase tracking-widest mb-2 font-geist font-normal transition-all duration-300">
+          <span className="backdrop-blur-lg  inline-flex items-center gap-2 uppercase tracking-widest text-xs font-medium mb-4 border-neutral-700 px-4 py-1.5 border rounded-full bg-gradient-to-tr from-teal-300/10 via-blue-400/10 to-orange-300/10 text-neutral-400 font-geist">
             Our Services
-          </h3>
+          </span>
           <p className="text-2xl text-neutral-200 font-light tracking-tight font-geist">
             We offer a range of services to help you achieve your goals.
           </p>
         </div>
 
         <div className="flex gap-8 items-center justify-evenly">
-          {[ShieldCheck, FileLock, GlobeLock, Award, Key].map((Icon, i) => (
-            <div
-              key={i}
-              className="group w-16 h-16 rounded-xl bg-neutral-900/50 border border-neutral-800 hover:border-neutral-700 flex items-center justify-center transition-all duration-300 hover:scale-110 backdrop-blur-lg"
-            >
-              <Icon className="w-7 h-7 text-neutral-600 group-hover:text-neutral-400 stroke-1.5 transition-colors" />
-            </div>
-          ))}
+          {[ShieldCheck, Network, BrainCircuit, Award, Handshake].map(
+            (Icon, i) => (
+              <div
+                key={i}
+                className="group w-16 h-16 rounded-xl bg-neutral-900/50 border border-neutral-800 hover:border-neutral-700 flex items-center justify-center transition-all duration-300 hover:scale-110 backdrop-blur-lg"
+              >
+                <Icon className="w-7 h-7 text-neutral-600 group-hover:text-neutral-400 stroke-1.5 transition-colors" />
+              </div>
+            )
+          )}
         </div>
 
         <div className="grid grid-cols-2 md:grid-cols-3 gap-6 mt-12 text-center">
